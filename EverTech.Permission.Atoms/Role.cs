6cf4c1bc6086c6582dc6074b41055ea62348b926
Jerry <728246447@qq.com>
2017-02-06 15:26:25 +0800

    role and user

--- EverTech.Permission.Atoms/Role.cs
+++ EverTech.Permission.Atoms/Role.cs
@@ -6,6 +6,12 @@ namespace EverTech.Permission.Atoms
 	public class Role
 	{
         public int Id { get; set; }
-
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public DateTime? AddTime { get; set; }
+        public DateTime? AddUser { get; set; }
+        public DateTime? EditTime { get; set; }
+        public int? EditUser { get; set; }
+        public bool? Enable { get; set; }
     }
 }
--- EverTech.Permission.Atoms/User.cs
+++ EverTech.Permission.Atoms/User.cs
@@ -6,6 +6,16 @@ namespace EverTech.Permission.Atoms
 	public class User
 	{
         public int Id { get; set; }
-
+        public string Account { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string EMail { get; set; }
+        public bool? Gender { get; set; }
+        public DateTime? AddTime { get; set; }
+        public DateTime? AddUser { get; set; }
+        public DateTime? EditTime { get; set; }
+        public int? EditUser { get; set; }
+        public bool? Enable { get; set; }
     }
 }
